82c5412be0bc83364603a89877a92d8c009e00c7
João V. Flores <joaoliveiraa7@gmail.com>
2023-07-05 17:58:01 -0300

    prisma client config

--- src/app/about/components/Trips.tsx
+++ src/app/about/components/Trips.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { prisma } from '@/lib/Prisma'; 
 
-const Trips = () => {
-  
-    return <div>Trips</div>
-  
+const getTrips = async () => {
+    const trips = await prisma.trip.findMany({});
+
+    return trips;
+};
+
+const Trips = async () => {
+    const data = await fetch("http://jsonplaceholder.typicode.com/posts").then((res) => res.json());
+    
+    return (
+        <div>
+            {data.map((i: any) => (
+               <p key={i.id}>{i.title}</p>
+            ))}
+        </div> 
+    )
 }
- export default Trips;
+
+export default Trips;
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const Prisma = 
+export const prisma = 
     globalForPrisma.prisma ||
     new PrismaClient({
         log: ["query"],
