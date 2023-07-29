ab8e5cd8794e2b32e7c2a256e3f5dbbdaa3017ee
João Oliveira <joaoliveiraa7@gmail.com>
2023-07-29 03:20:02 -0300

    feat: add trip highlights component

---
+++ src/app/trips/[tripId]/components/TripHighlights.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image';
+
+interface TripHighlightsProps {
+    highlights: string[];
+};
+
+const TripHighlights = ({highlights}: TripHighlightsProps) => {
+  return (
+    <div className='flex flex-col p-5'>
+        <h2 className='font-semibold mb-3 text-primaryDark'>Destaques</h2>
+        
+        <div className="flex flex-wrap gap-y-3">
+            {highlights.map((highlight, index) => (
+                <div key={highlight} className="flex items-center  gap-2 w-1/2">
+                    <Image src="/check-icon2.png" alt={highlight} width={15} height={15}/>
+                
+                    <p className="text-grayPrimary text-xs">{highlight}</p>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default TripHighlights
