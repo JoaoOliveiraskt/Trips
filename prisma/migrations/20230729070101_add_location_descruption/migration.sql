28095a79c08c43ab6d7205fbfb6995986fea0080
João Oliveira <joaoliveiraa7@gmail.com>
2023-07-29 18:47:25 -0300

    feat: add location description

---
+++ prisma/migrations/20230729070101_add_location_descruption/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl nec ultricies ultrices, nunc nisl aliquam nunc, nec aliquam nunc nisl nec nisl.';
